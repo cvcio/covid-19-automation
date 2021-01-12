8de0ecebbff8eb8ad08a265f071868aae8cd02d9
andefined <dimitris.papaevagelou@andefined.com>
2021-01-12 13:30:04 +0200

    added gr_vaccines/govgr strategy, various fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,7 @@ exit_code=0
 if [[ ! -v MONGO_URL ]]; then
     MONGO_URL="mongodb://localhost:27017/"
 fi
-python src/covid-19.py 
-    --log-level info 
-    --source all 
-    --drop true 
-    --clone true 
-    --mongo $MONGO_URL 
-    --govgr_token $GOVGR_TOKEN
+python src/covid-19.py --log-level info --source all --drop true --clone true --mongo $MONGO_URL --govgr_token $GOVGR_TOKEN
 
 echo "$0 finished with code $exit_code."
 exit $exit_code
